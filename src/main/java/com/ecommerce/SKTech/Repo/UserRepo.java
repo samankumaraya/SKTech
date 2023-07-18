bb3623ed4e6b7a7b10f1bd29e043b2d8b6b7821c
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-07-18 19:55:10 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/SKTech/Repo/UserRepo.java
@@ -0,0 +1,15 @@
+//package com.ecommerce.SKTech.Repo;
+//
+//import com.ecommerce.SKTech.model.user;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//@EnableJpaRepositories
+//@Component
+//public interface UserRepo extends JpaRepository<user,Long> {
+//
+//
+//}
---
+++ src/main/java/com/ecommerce/SKTech/SkTechApplication.java
@@ -0,0 +1,16 @@
+package com.ecommerce.SKTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class SkTechApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkTechApplication.class, args);
+	}
+
+}
